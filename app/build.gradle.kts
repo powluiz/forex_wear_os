bc298d032ccc5c45a14422661f4c5b07a26a64f6
Luiz Ricardo Brumati De Lima <luiz.1303@hotmail.com>
2025-11-04 23:27:28 -0300

    build: update target sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.powluiz.forexwearos"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.powluiz.forexwearos"
         minSdk = 34
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
